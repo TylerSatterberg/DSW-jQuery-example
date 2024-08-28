72ea8375ced2295c3ef66c511a13a2df3d462c04
tylersatterberg <satterbergtyler@gmail.com>
2024-08-28 13:48:46 -0700

    test1

--- myscript.js
+++ myscript.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
-	$("h1").click(function() {
+	$("h1").click(function() 
+	{
 		$("p").addClass("bright");
 		$("img").addClass("border");
 	});
+	$("disappear2").click(function()
+	{
+		$("p1").toggle(p2 p3)
+	}
 });
